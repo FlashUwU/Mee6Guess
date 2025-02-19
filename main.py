6cab10a207f21a1b5c72e7941d50926020acd5b8
FlashUwU <iamflashuwu@gmail.com>
2025-02-19 12:38:40 +0800

    Updated Something.

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ risky_chop = 50
 number_of_piece = 10
 weights: dict[int, int] = {} # Answer: Frequency
 
-data = [0]*number_of_piece
+dict_data: dict[float, list[int]] = {}
+list_data = [0]*number_of_piece
 base_under = 100 / number_of_piece
 
 for answer in notes["answers"]:
@@ -27,7 +28,10 @@ for answer in notes["answers"]:
     bu = base_under
     for index in range(number_of_piece):
         if answer <= bu:
-            data[index] += 1
+            list_data[index] += 1
+
+            try: dict_data[bu].append(answer)
+            except: dict_data[bu] = [answer]
             break
         
         bu += base_under
@@ -43,4 +47,5 @@ print(f"""=============RESULT=============
 鎖定金額: {lock}
 共賺了：{earned}
 
-落點統計：{data}""")
+落點統計：{list_data}
+分佈圖：{dict(sorted(dict_data.items()))}""")
